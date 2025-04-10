66973a2369b8c3194953a9e10492e27124674b2e
choiruladamm <choiruladamm@gmail.com>
2025-04-11 00:25:10 +0700

    feat: init shadcnui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
 import React from 'react';
 
 interface HomePageProps {}
 
 const HomePage: React.FC<HomePageProps> = ({}) => {
-	return <div>HomePage</div>;
+	return (
+		<div className='grid min-h-dvh place-items-center'>
+			<div className='flex gap-2'>
+				<Button>shadcnui</Button>
+				<Input placeholder='Search...' />
+			</div>
+		</div>
+	);
 };
 
 export default HomePage;
