c04e4b444c0e5a65e831fc2763e44d09f7b2648a
choiruladamm <choiruladamm@gmail.com>
2025-04-11 20:52:12 +0700

    refactor(todos): optimize getFilteredTodos with createSelector
    - improved selector performance by memoizing filtered todos

--- src/features/todos/store/selectors.ts
+++ src/features/todos/store/selectors.ts
@@ -1,20 +1,23 @@
 import { RootState } from '@/stores';
 import { Todo } from './todo-slice';
+import { createSelector } from '@reduxjs/toolkit';
 
-export const getFilteredTodos = (state: RootState): Todo[] => {
-	const { todos, filter, search } = state.todos;
-	const keyword = search.toLowerCase().trim();
+export const getFilteredTodos = createSelector(
+	(state: RootState) => state.todos,
+	(todosState): Todo[] => {
+		const keyword = todosState.search.toLowerCase().trim();
 
-	let filtered = todos;
+		let filtered = todosState.todos;
 
-	switch (filter) {
-		case 'active':
-			filtered = todos.filter(t => !t.completed);
-			break;
-		case 'completed':
-			filtered = todos.filter(t => t.completed);
-			break;
-	}
+		switch (todosState.filter) {
+			case 'active':
+				filtered = todosState.todos.filter(t => !t.completed);
+				break;
+			case 'completed':
+				filtered = todosState.todos.filter(t => t.completed);
+				break;
+		}
 
-	return filtered.filter(t => t.text.toLowerCase().includes(keyword));
-};
+		return filtered.filter(t => t.text.toLowerCase().includes(keyword));
+	},
+);
